0ef6e9c5680aef2bdee1ced1608318d093980e0c
Iván Vergara <ivl@uc.cl>
2023-07-04 01:53:21 -0400

    update: matlab code

--- Proyecto/code/main.m
+++ Proyecto/code/main.m
@@ -1,6 +1,18 @@
-data=loadjson('data/BrainWeb_Subject04.jmsh')
+data=loadjson('data/BrainWeb_Subject05.jmsh')
 
-% plotmesh(data.MeshNode, data.MeshElem, 'x<100');
-plotmesh(data.MeshNode, data.MeshElem(data.MeshElem(:,5)==4,:),data.MeshElem(data.MeshElem(:,5)==5,:));
-% xmlmesh(data.MeshNode, data.MeshElem(data.MeshElem(:,5)==4,:), 'hola.xml')
-% stlwrite(data.MeshElem, 'filename.stl');
+% Obtain Head sections from data
+Scalp = data.MeshElem(data.MeshElem(:,5)==1,:);
+Skull = data.MeshElem(data.MeshElem(:,5)==2,:);
+CSF = data.MeshElem(data.MeshElem(:,5)==3,:);   % Cerebrospinal fluid
+GM = data.MeshElem(data.MeshElem(:,5)==4,:);    % Grey matter
+WM = data.MeshElem(data.MeshElem(:,5)==5,:);    % White matter
+
+% Plot GM and WM
+plotmesh(data.MeshNode, GM, WM);
+
+% Save head sections as different msh files
+savemsh(data.MeshNode, Scalp, 'msh_files/01_Scalp.msh')
+savemsh(data.MeshNode, Skull, 'msh_files/02_Skull.msh')
+savemsh(data.MeshNode, CSF, 'msh_files/03_CSF.msh')
+savemsh(data.MeshNode, GM, 'msh_files/04_GM.msh')
+savemsh(data.MeshNode, WM, 'msh_files/05_WM.msh')
